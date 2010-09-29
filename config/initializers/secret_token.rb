1160c26f312042b40c244d8658d9bbd0ee43c0de
pushmatrix <daniel.beauchamp@gmail.com>
2010-09-29 18:39:00 -0400

    Initial open sourcing of apps4ottawa.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apps4ottawa::Application.config.secret_token = 'a55045b22f560e1411777e6dbcc7be2e6265ee02e5b9688f7b4a77634347c808daf46bd6d5db548bec81b7e32cab1d052c6e85a2e673e3aa65e1d715ef51347c'
